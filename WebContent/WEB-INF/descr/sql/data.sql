8a745cbc580c631494068c21f95e5f63e66ca0d5
Xennis <xennis@online.de>
2014-05-08 14:49:06 +0200

    Handle logout, init database

---
+++ WebContent/WEB-INF/descr/sql/data.sql
@@ -0,0 +1,14 @@
+-- ---------- POPUPLATE TABLE `MODULE` ----------
+INSERT INTO "APP"."MODULE" ("OID", "MODULEID", "MODULENAME") VALUES (1, 'sv3', 'Administrator');
+INSERT INTO "APP"."MODULE" ("OID", "MODULEID", "MODULENAME") VALUES (2, 'sv1', 'Site');
+
+-- ---------- POPUPLATE TABLE "GROUP" ----------
+INSERT INTO "APP"."GROUP" ("OID", "GROUPNAME", "MODULE_OID") VALUES (1, 'Administrators', 1);
+INSERT INTO "APP"."GROUP" ("OID", "GROUPNAME", "MODULE_OID") VALUES (2, 'Customers', 2);
+
+-- ---------- POPUPLATE TABLE "USER" ----------
+INSERT INTO "APP"."USER" ("OID", "USERNAME", "PASSWORD", "EMAIL", "GROUP_OID") VALUES (1, 'manager', 'manager', 'manager@acme.com', 1);
+
+-- ---------- POPUPLATE TABLE "MEMBERSHIP" ----------
+INSERT INTO "APP"."MEMBERSHIP" ("OID", "MEMBERSHIPNAME") VALUES (1, 'Traveller');
+INSERT INTO "APP"."MEMBERSHIP" ("OID", "MEMBERSHIPNAME") VALUES (2, 'Business');
