6e23142787f9248965fe59e4786a33c1f4f2c4f5
Xennis <xennis@online.de>
2014-05-11 12:56:46 +0200

    Add review

--- DBScripts/structure.sql
+++ DBScripts/structure.sql
@@ -18,14 +18,14 @@ create table "APP"."MODULE" (
 -- User [User]
 create table "APP"."USER" (
    "OID"  integer  not null,
-   "USERNAME"  varchar(255),
-   "PASSWORD"  varchar(255),
    "EMAIL"  varchar(255),
-   "MEMBERSHIP"  varchar(255),
+   "PASSWORD"  varchar(255),
    "NAME"  varchar(255),
    "BITH_DATE"  date,
    "ADDRESS"  varchar(255),
    "INTERESTS"  clob(10000),
+   "MEMBERSHIP"  varchar(255),
+   "USERNAME"  varchar(255),
   primary key ("OID")
 );
 
@@ -35,6 +35,7 @@ create table "APP"."REVIEW" (
    "OID"  integer  not null,
    "MESSAGE"  clob(10000),
    "IMAGE"  varchar(255),
+   "RATING"  varchar(255),
   primary key ("OID")
 );
 
@@ -42,6 +43,7 @@ create table "APP"."REVIEW" (
 -- Group_DefaultModule [Group2DefaultModule_DefaultModule2Group]
 alter table "APP"."GROUP"  add column  "MODULE_OID"  integer;
 alter table "APP"."GROUP"   add constraint FK_GROUP_MODULE foreign key ("MODULE_OID") references "APP"."MODULE" ("OID");
+create index "IDX_GROUP_MODULE" on "APP"."GROUP"("MODULE_OID");
 
 
 -- Group_Module [Group2Module_Module2Group]
@@ -52,11 +54,14 @@ create table "APP"."GROUP_MODULE" (
 );
 alter table "APP"."GROUP_MODULE"   add constraint FK_GROUP_MODULE_GROUP foreign key ("GROUP_OID") references "APP"."GROUP" ("OID");
 alter table "APP"."GROUP_MODULE"   add constraint FK_GROUP_MODULE_MODULE foreign key ("MODULE_OID") references "APP"."MODULE" ("OID");
+create index "IDX_GROUP_MODULE_GROUP" on "APP"."GROUP_MODULE"("GROUP_OID");
+create index "IDX_GROUP_MODULE_MODULE" on "APP"."GROUP_MODULE"("MODULE_OID");
 
 
 -- User_DefaultGroup [User2DefaultGroup_DefaultGroup2User]
 alter table "APP"."USER"  add column  "GROUP_OID"  integer;
 alter table "APP"."USER"   add constraint FK_USER_GROUP foreign key ("GROUP_OID") references "APP"."GROUP" ("OID");
+create index "IDX_USER_GROUP" on "APP"."USER"("GROUP_OID");
 
 
 -- User_Group [User2Group_Group2User]
@@ -67,10 +72,13 @@ create table "APP"."USER_GROUP" (
 );
 alter table "APP"."USER_GROUP"   add constraint FK_USER_GROUP_USER foreign key ("USER_OID") references "APP"."USER" ("OID");
 alter table "APP"."USER_GROUP"   add constraint FK_USER_GROUP_GROUP foreign key ("GROUP_OID") references "APP"."GROUP" ("OID");
+create index "IDX_USER_GROUP_USER" on "APP"."USER_GROUP"("USER_OID");
+create index "IDX_USER_GROUP_GROUP" on "APP"."USER_GROUP"("GROUP_OID");
 
 
 -- User_Review [rel1]
 alter table "APP"."REVIEW"  add column  "USER_OID"  integer;
 alter table "APP"."REVIEW"   add constraint FK_REVIEW_USER foreign key ("USER_OID") references "APP"."USER" ("OID");
+create index "IDX_REVIEW_USER" on "APP"."REVIEW"("USER_OID");
 
 
